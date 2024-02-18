6544754e1d14b7049f2777b4a9ff5b4be0f9d5b7
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-18 20:47:05 +0700

    feat: a new course to be introduced with if expression in kotlin

---
+++ memulai-pemrograman-dengan-kotlin/kotlin-fundamental/src/main/kotlin/if_expressions/Main.kt
@@ -0,0 +1,23 @@
+package if_expressions
+
+fun main() {
+    /**
+     * if expressions
+     * basically this is used to make a decision
+     * and this expressions return boolean, therefore it can be assigned in variable
+     */
+    val openHours = 7
+    val now = 7
+    val office: String = if (now > 7) {
+        "Office already open"
+    } else if (now == openHours){
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    print(office)
+    /*
+        output : Wait a minute, office will be open
+     */
+}
