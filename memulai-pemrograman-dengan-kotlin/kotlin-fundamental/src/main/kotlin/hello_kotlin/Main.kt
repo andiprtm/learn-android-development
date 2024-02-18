9458f01a028a260bfa99dd53975f5930fe868a48
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-18 17:12:08 +0700

    feat: a new course to be introduced with kotlin

---
+++ memulai-pemrograman-dengan-kotlin/kotlin-fundamental/src/main/kotlin/hello_kotlin/Main.kt
@@ -0,0 +1,24 @@
+package hello_kotlin
+
+// main function -> this is a comment. the comment will not be compiled when the code is executed
+fun main() {
+    // to print some text use print() or println()
+    // The difference is that print() will print an argument on the same line
+    // and println() stands for print line will print the argument to the next line
+
+    // print()
+    print("Hello Kotlin!")
+    print("Hello Andi!")
+    // output : Hello Kotlin!Hello Andi!
+
+    // println()
+    println("Hello Kotlin!")
+    println("Hello Andi")
+    /*
+    * multi-line comment
+    * output:
+    * Hello Kotlin!
+    * Hello Andi!
+    * */
+
+}
