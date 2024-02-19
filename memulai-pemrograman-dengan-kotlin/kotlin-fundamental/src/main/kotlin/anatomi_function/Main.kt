f85817bd7b279c2ceb63802b3ccab16425b2479d
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-19 18:33:29 +0700

    feat: a new course to be introduced with anatomy function in kotlin

---
+++ memulai-pemrograman-dengan-kotlin/kotlin-fundamental/src/main/kotlin/anatomi_function/Main.kt
@@ -0,0 +1,26 @@
+package anatomi_function
+
+fun main() {
+    /**
+     * function anatomy
+     *
+     * - Function Header
+     * private fun sum(number: Int, number2: Int): Int
+     *
+     * private = visibility modifier, if not declare the default set to public
+     * fun sum = function name
+     * (number: Int, number2: Int) = function parameter
+     * : Int = function return type
+     *
+     * - Function Body
+     * {
+     *     return number + number2
+     * }
+     *
+     *
+     */
+}
+
+private fun sum(number: Int, number2: Int): Int{
+    return number + number2
+}
