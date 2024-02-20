76920d342d2cdc2ad41625372fa870be3b1e28d2
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-20 20:55:08 +0700

    fix: add lazy explanation

--- memulai-pemrograman-dengan-kotlin/kotlin_oop/src/main/kotlin/lateinit_and_lazy_property/Main.kt
+++ memulai-pemrograman-dengan-kotlin/kotlin_oop/src/main/kotlin/lateinit_and_lazy_property/Main.kt
@@ -1,11 +1,16 @@
 package lateinit_and_lazy_property
 
 lateinit var name: String
+
+val fullName: String by lazy {
+    "Dicoding Miaw"
+}
 fun main() {
     /**
      * this feature helps you to initialize the variable later
      * the different is "lateinit" always use var,
      * and the "lazy" always use val
+     * the lazy property is used to initialize the variable when it really needed
      */
     lateinit var age: String
     name = "Dicoding Miaw"
@@ -21,4 +26,6 @@ fun main() {
     /*
         output: property name (Kotlin reflection is not available)
      */
+
+    println(fullName.length)
 }
