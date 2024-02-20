db0125d5682efe610b95a6557573b80748bc2524
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-20 22:45:05 +0700

    feat: a new course to be introduced with modifier in kotlin

---
+++ memulai-pemrograman-dengan-kotlin/kotlin_oop/src/main/kotlin/modifier/Main.kt
@@ -0,0 +1,15 @@
+package modifier
+
+fun main() {
+    /**
+     * Here are the kinds of access rights and a brief explanation that can be used in Kotlin:
+     *
+     * Public: The access right with the broadest scope. Members with this modifier can be accessed from anywhere.
+     *
+     * Private: The access right with the most limited scope. Members to which it applies can only be accessed within the same scope. Usually in the same file
+     *
+     * Protected: Access rights whose scope is limited to the class hierarchy. Members can only be accessed on their child classes or the class itself.
+     *
+     * Internal: An access right whose scope is limited to a single module. Members who use it cannot be accessed outside of that module. Usually in the same folder
+     */
+}
