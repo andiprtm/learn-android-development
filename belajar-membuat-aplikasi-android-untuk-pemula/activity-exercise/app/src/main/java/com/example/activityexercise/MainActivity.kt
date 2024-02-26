ac08fbd0de61300a459dea9af32c5b657c3c4b34
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-26 14:48:07 +0700

    feat: adds logic to save a state

--- belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/src/main/java/com/example/activityexercise/MainActivity.kt
+++ belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/src/main/java/com/example/activityexercise/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.activityexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -26,6 +27,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         tvResult = findViewById(R.id.tv_result)
 
         btnCalculate.setOnClickListener(this)
+
+        if(savedInstanceState != null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
     }
 
     override fun onClick(v: View?) {
@@ -34,8 +40,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val inputLength = edtLength.text.toString().trim()
             val inputWidth = edtWidth.text.toString().trim()
             val inputHeight = edtHeight.text.toString().trim()
-            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-            tvResult.text = volume.toString()
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                edtLength.error = "This field cannot be empty"
+            }
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = "This field cannot be empty"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                edtHeight.error = "This field cannot be empty"
+            }
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+            }
         }
+    }
+
+    // this method is running before an activity is killed
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+
+    // this companion object is used if you want to access this property without create the instance of this class
+    // you can access the STATE_RESULT in other class/file with MainActivity.STATE_RESULT
+    companion object{
+        private const val STATE_RESULT = "state_result"
     }
 }
