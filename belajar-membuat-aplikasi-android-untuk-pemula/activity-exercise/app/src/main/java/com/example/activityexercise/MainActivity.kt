d5b3be36ea932caff7399ad5e79548da51b2d3e8
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-26 14:49:01 +0700

    fix: delete unused import

--- belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/src/main/java/com/example/activityexercise/MainActivity.kt
+++ belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/src/main/java/com/example/activityexercise/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.activityexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
