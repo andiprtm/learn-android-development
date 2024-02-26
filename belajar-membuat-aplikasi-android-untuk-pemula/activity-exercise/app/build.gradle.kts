3696f2365a5bc44ec755aed5b602ca0b49049e55
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-26 13:34:29 +0700

    fix: add string resource variable in main_layout.xml

--- belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/build.gradle.kts
+++ belajar-membuat-aplikasi-android-untuk-pemula/activity-exercise/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.activityexercise"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.activityexercise"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
