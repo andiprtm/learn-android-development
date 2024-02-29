e3c8e95cffb06326386fcc51e90402179d81ab6a
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-29 14:07:20 +0700

    feat: add TextView id on MoveWithDataActivity.kt

--- belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithDataActivity.kt
+++ belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithDataActivity.kt
@@ -2,10 +2,16 @@ package com.example.explicitintentexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MoveWithDataActivity : AppCompatActivity() {
+
+    private lateinit var tvDataReceived: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_data)
+
+        tvDataReceived = findViewById(R.id.tv_data_received)
     }
 }
