dfd26de523a0b0f5434c810ced4b1ee4c809a1b1
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-29 15:31:22 +0700

    feat: add tv_object receiver

--- belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithObjectActivity.kt
+++ belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithObjectActivity.kt
@@ -2,10 +2,16 @@ package com.example.explicitintentexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MoveWithObjectActivity : AppCompatActivity() {
+
+    private lateinit var tvObject: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_object)
+
+        tvObject = findViewById(R.id.tv_object_received)
     }
 }
