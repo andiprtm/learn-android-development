69584ccf08561db056988a168e9e99fbf54ca9af
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-02-29 14:13:02 +0700

    feat: add logic to get data from intent

--- belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithDataActivity.kt
+++ belajar-membuat-aplikasi-android-untuk-pemula/explicit-intent-exercise/app/src/main/java/com/example/explicitintentexercise/MoveWithDataActivity.kt
@@ -13,5 +13,16 @@ class MoveWithDataActivity : AppCompatActivity() {
         setContentView(R.layout.activity_move_with_data)
 
         tvDataReceived = findViewById(R.id.tv_data_received)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        val text = "Name : $name, Age : $age"
+        tvDataReceived.text = text
+    }
+
+    companion object{
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
     }
 }
