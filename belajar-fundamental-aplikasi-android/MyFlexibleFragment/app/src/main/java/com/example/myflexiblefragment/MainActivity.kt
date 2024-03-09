e680509c972dc4a62e0b06771dc3a7f216430756
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-09 14:25:09 +0700

    feat: binding fragment home to main activity

--- belajar-fundamental-aplikasi-android/MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/MainActivity.kt
+++ belajar-fundamental-aplikasi-android/MyFlexibleFragment/app/src/main/java/com/example/myflexiblefragment/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myflexiblefragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.myflexiblefragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment){
+            Log.d("MyFlexibleFragment", "Fragment Name : ${HomeFragment::class.java.simpleName}")
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
     }
 }
