c8a31ac26e8a3650fbe4a13e63c102b861085c1f
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-12 08:27:13 +0700

    feat: create two menu in app bar

--- belajar-fundamental-aplikasi-android/MyAppBar/app/src/main/java/com/example/myappbar/MainActivity.kt
+++ belajar-fundamental-aplikasi-android/MyAppBar/app/src/main/java/com/example/myappbar/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.myappbar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myappbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId){
+                R.id.menu_1 -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragment_container, MenuFragment())
+                        addToBackStack(null)
+                        commit()
+                    }
+                    true
+                }
+
+                R.id.menu_2 ->{
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
