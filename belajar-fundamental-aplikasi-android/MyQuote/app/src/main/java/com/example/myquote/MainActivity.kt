8de246cd6a4ad7251a518323e6fbd3af5086f064
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-15 21:42:35 +0700

    feat: implement loopj to display array of object from api

--- belajar-fundamental-aplikasi-android/MyQuote/app/src/main/java/com/example/myquote/MainActivity.kt
+++ belajar-fundamental-aplikasi-android/MyQuote/app/src/main/java/com/example/myquote/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myquote
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -30,6 +31,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         getRandomQuote()
+
+        binding.btnAllQuotes.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ListQuotesActivity::class.java))
+        }
     }
 
     private fun getRandomQuote() {
---
+++ belajar-fundamental-aplikasi-android/MyQuote/app/src/main/java/com/example/myquote/QuoteAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.myquote
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myquote.databinding.ItemQuoteBinding
+
+class QuoteAdapter (private val listReview: ArrayList<String>): RecyclerView.Adapter<QuoteAdapter.ViewHolder>() {
+    class ViewHolder(var binding: ItemQuoteBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemQuoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return listReview.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.tvItem.text = listReview[position]
+    }
+}
