bd0248e96f926107b131f150c27af3df2b731078
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-16 19:30:45 +0700

    feat: final

--- belajar-fundamental-aplikasi-android/RestaurantReview/app/src/main/java/com/example/restaurantreview/MainActivity.kt
+++ belajar-fundamental-aplikasi-android/RestaurantReview/app/src/main/java/com/example/restaurantreview/MainActivity.kt
@@ -1,20 +1,140 @@
 package com.example.restaurantreview
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.example.restaurantreview.data.response.CustomerReviewsItem
+import com.example.restaurantreview.data.response.Response
+import com.example.restaurantreview.data.response.ResponsePost
+import com.example.restaurantreview.data.response.Restaurant
+import com.example.restaurantreview.data.retrofit.ApiConfig
+import com.example.restaurantreview.databinding.ActivityMainBinding
+import com.example.restaurantreview.ui.ReviewAdapter
+import retrofit2.Call
+import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        supportActionBar?.hide()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvReview.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.rvReview.addItemDecoration(itemDecoration)
+
+        findRestaurant()
+
+        binding.btnSend.setOnClickListener { view ->
+            postReview(binding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun postReview(input: String) {
+        showLoading(true)
+
+        val client = ApiConfig.GetApiService().postReview(RESTAURANT_ID, "Dicoding", input)
+
+        client.enqueue(object : Callback<ResponsePost> {
+            override fun onResponse(
+                call: Call<ResponsePost>,
+                response: retrofit2.Response<ResponsePost>
+            ) {
+                showLoading(false)
+
+                val responBody = response.body()
+
+                if (response.isSuccessful && responBody != null) {
+                    setReviewData(responBody.customerReviews)
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<ResponsePost>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+    }
+
+    private fun findRestaurant() {
+        showLoading(true)
+
+        val client = ApiConfig.GetApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : Callback<Response> {
+            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                showLoading(false)
+
+                if (response.isSuccessful) {
+                    val bodyResponse = response.body()
+
+                    if (bodyResponse != null) {
+                        setDataRestaurant(bodyResponse.restaurant)
+                        setReviewData(bodyResponse.restaurant.customerReviews)
+                    }
+                } else {
+                    Log.d(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Response>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+    }
+
+    private fun setReviewData(customerReviews: List<CustomerReviewsItem>) {
+        val adapter = ReviewAdapter()
+        adapter.submitList(customerReviews)
+        binding.rvReview.adapter = adapter
+        binding.edReview.setText("")
+    }
+
+    private fun setDataRestaurant(restaurant: Restaurant) {
+        binding.tvTitle.text = restaurant.name
+        binding.tvDescription.text = restaurant.description
+        Glide.with(this@MainActivity)
+            .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+            .into(binding.ivPicture)
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.INVISIBLE
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
     }
 }
