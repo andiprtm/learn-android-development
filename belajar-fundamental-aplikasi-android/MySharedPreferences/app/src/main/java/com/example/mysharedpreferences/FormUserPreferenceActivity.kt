02e31cb5b91f4d9224d369a88fbb4404aacbe139
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-24 15:55:59 +0700

    init: add FormUserPreferenceActivity

---
+++ belajar-fundamental-aplikasi-android/MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/FormUserPreferenceActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mysharedpreferences
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FormUserPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_user_preference)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
