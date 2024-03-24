86d311b703bbe258d26cf1795f421169ef915ffb
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-24 15:54:27 +0700

    feat: create user preference model

--- belajar-fundamental-aplikasi-android/MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/UserModel.kt
+++ belajar-fundamental-aplikasi-android/MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/UserModel.kt
@@ -7,7 +7,7 @@ import kotlinx.parcelize.Parcelize
 data class UserModel(
     var name: String? = null,
     var email: String? = null,
-    var age: Int? = null,
-    val phoneNumber: String? = null,
-    val isLove: Boolean? = false
+    var age: Int = 0,
+    var phoneNumber: String? = null,
+    var isLove: Boolean = false
 ): Parcelable
