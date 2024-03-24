f3110fbbb97acd7423b28dcaedc8666ea39996e4
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-24 20:14:38 +0700

    feat: create setting preference display

---
+++ belajar-fundamental-aplikasi-android/MySettingPreference/app/src/main/java/com/example/mysettingpreference/MyPreferenceFragment.kt
@@ -0,0 +1,10 @@
+package com.example.mysettingpreference
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class MyPreferenceFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preference)
+    }
+}
