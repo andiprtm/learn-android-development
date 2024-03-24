e9d6b5906bc8ae53011f7af13e0a64ec59634e52
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-24 13:48:27 +0700

    feat: done

--- belajar-fundamental-aplikasi-android/MyReadWriteFile/app/src/main/java/com/example/myreadwritefile/FileHelper.kt
+++ belajar-fundamental-aplikasi-android/MyReadWriteFile/app/src/main/java/com/example/myreadwritefile/FileHelper.kt
@@ -4,17 +4,17 @@ import android.content.Context
 
 internal object FileHelper {
 
-    fun writeToFile(fileModel: FileModel, context: Context){
+    fun writeToFile(fileModel: FileModel, context: Context) {
         context.openFileOutput(fileModel.fileName, Context.MODE_PRIVATE).use {
             it.write(fileModel.data?.toByteArray())
         }
     }
 
-    fun readFromFile(context: Context, fileName: String): FileModel{
+    fun readFromFile(context: Context, fileName: String): FileModel {
         val fileModel = FileModel()
         fileModel.fileName = fileName
         fileModel.data = context.openFileInput(fileName).bufferedReader().useLines { lines ->
-            lines.fold(""){some, text ->
+            lines.fold("") { some, text ->
                 "$some$text\n"
             }
         }
