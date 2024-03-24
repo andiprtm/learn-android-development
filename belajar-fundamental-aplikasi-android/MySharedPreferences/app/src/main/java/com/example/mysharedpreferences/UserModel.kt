7425e986b81822f9651fb3f62d1ea19a7ced1fa7
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-24 15:18:16 +0700

    feat: implement parcelable in UserModel data class

--- belajar-fundamental-aplikasi-android/MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/UserModel.kt
+++ belajar-fundamental-aplikasi-android/MySharedPreferences/app/src/main/java/com/example/mysharedpreferences/UserModel.kt
@@ -1,9 +1,13 @@
 package com.example.mysharedpreferences
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class UserModel(
     var name: String? = null,
     var email: String? = null,
     var age: Int? = null,
     val phoneNumber: String? = null,
     val isLove: Boolean? = false
-)
+): Parcelable
