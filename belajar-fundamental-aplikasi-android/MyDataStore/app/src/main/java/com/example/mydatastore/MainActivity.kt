78ebf1f36ec37ccc75a6914aa865b62acd9983e5
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-03-25 13:11:19 +0700

    feat: create ui logic in main activity

--- belajar-fundamental-aplikasi-android/MyDataStore/app/src/main/java/com/example/mydatastore/MainActivity.kt
+++ belajar-fundamental-aplikasi-android/MyDataStore/app/src/main/java/com/example/mydatastore/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.example.mydatastore
 
 import android.os.Bundle
+import android.widget.CompoundButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mydatastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val switchTheme = binding.switchTheme
+
+        switchTheme.setOnCheckedChangeListener{_: CompoundButton?, isChecked: Boolean ->
+            if (isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
         }
     }
 }
