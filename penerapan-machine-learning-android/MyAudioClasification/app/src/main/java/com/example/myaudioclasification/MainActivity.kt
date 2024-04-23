f5f4dcfeaf0d0fa5c470f9e8ef8bb7ba7cfb0ba3
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-04-23 10:19:26 +0700

    feat: add audio classification feature

--- penerapan-machine-learning-android/MyAudioClasification/app/src/main/java/com/example/myaudioclasification/MainActivity.kt
+++ penerapan-machine-learning-android/MyAudioClasification/app/src/main/java/com/example/myaudioclasification/MainActivity.kt
@@ -1,20 +1,124 @@
 package com.example.myaudioclasification
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myaudioclasification.databinding.ActivityMainBinding
+import com.google.mediapipe.tasks.components.containers.Classifications
+import java.text.NumberFormat
+import android.Manifest
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var audioClassifierHelper: AudioClassifierHelper
+    private var isRecording = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeAudioClassifierHelper()
+        setClickListener()
+        updateButtonState()
+        requestPermissionsIfNeeded()
+    }
+
+    private fun requestPermissionsIfNeeded() {
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
+    }
+
+    private fun allPermissionsGranted()=
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            val message = if (isGranted) "Permission granted" else "Permission denied"
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        }
+
+    private fun updateButtonState() {
+        binding.btnStart.isEnabled = !isRecording
+        binding.btnStop.isEnabled = isRecording
+    }
+
+    private fun setClickListener() {
+        binding.btnStart.setOnClickListener {
+            audioClassifierHelper.startAudioClassification()
+            isRecording = true
+            updateButtonState()
+        }
+        binding.btnStop.setOnClickListener {
+            audioClassifierHelper.stopAudioClassification()
+            isRecording = false
+            updateButtonState()
+        }
+    }
+
+    private fun initializeAudioClassifierHelper() {
+        audioClassifierHelper = AudioClassifierHelper(
+            context = this,
+            classifierListener = object : AudioClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResult(result: List<Classifications>, inferenceTime: Long) {
+                    runOnUiThread{
+                        result.let { it ->
+                            if (it.isEmpty() && it[0].categories().isEmpty()){
+                                println(it)
+                                val sortedCategories =
+                                    it[0].categories().sortedByDescending { it?.score() }
+                                val displayResult =
+                                    sortedCategories.joinToString("\n") {
+                                        "${it.categoryName()} " + NumberFormat.getPercentInstance()
+                                            .format(it.score()).trim()
+                                    }
+                                binding.tvResult.text = displayResult
+                            } else {
+                                binding.tvResult.text = ""
+                            }
+                        }
+                    }
+                }
+
+            }
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (isRecording) {
+            audioClassifierHelper.startAudioClassification()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (::audioClassifierHelper.isInitialized) {
+            audioClassifierHelper.stopAudioClassification()
+        }
+    }
+
+    companion object {
+        private const val REQUIRED_PERMISSION = Manifest.permission.RECORD_AUDIO
     }
 }
