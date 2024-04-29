c0da8156d580a81c862bfec864e1b56af0e94e45
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2024-04-29 10:53:48 +0700

    feat: add MyEditText class to build custom edit text

---
+++ belajar-pengembangan-aplikasi-android-intermedieate/MyCustomView/app/src/main/java/com/example/mycustomview/MyEditText.kt
@@ -0,0 +1,12 @@
+package com.example.mycustomview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class MyEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+
+
+}
